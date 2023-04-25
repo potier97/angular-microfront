2375d36a8cac7f6446b7c79fb72576c450996e74
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2023-04-24 20:29:08 -0500

    change-routing-file

--- projects/mf-shell/src/app/app-routing.module.ts
+++ projects/mf-shell/src/app/app-routing.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { loadRemoteModule } from '@angular-architects/module-federation';
-
 const routes: Routes = [
   {
     path: '',
-    // loadChildren: () => import('mfShopping/ProductsModule').then((m) => m.ProductsModule),
-    loadChildren: () => 
-         loadRemoteModule({
-           type: 'module',
-           remoteEntry: 'http://localhost:4210/remoteEntry.js',
-           exposedModule: './ProductsModule',
-         })
-         .then(m => m.ProductsModule)
+    loadChildren: () => import('mfShopping/ProductsModule').then((m) => m.ProductsModule),
   },
   {
     path: 'payment',
