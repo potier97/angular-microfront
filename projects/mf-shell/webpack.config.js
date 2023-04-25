9306dd4468670b2657ce5da634fce8ec1680e1a0
Nicolas Potier Anzola <nicolaspotier97@gmail.com>
2023-04-24 21:31:00 -0500

    update-remoe-entry-url-v2

--- projects/mf-shell/webpack.config.js
+++ projects/mf-shell/webpack.config.js
@@ -1,13 +1,22 @@
-const { environment } = require('./src/environments/environment');
+// const { environment } = require('./src/environments/environment');
 const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
 
+const mfPaymentsUrl = process.env.MF_PAYMENTS_URL || 'http://localhost:4208';
+const mfShoppingUrl = process.env.MF_SHOPPING_URL || 'http://localhost:4210';
 
 module.exports = withModuleFederationPlugin({
 
   remotes: {
-    "mfPayments": `${environment.urlMf1}/remoteEntry.js`,
-    "mfShopping": `${environment.urlMf1}/remoteEntry.js`,    
+    // "mfPayments": `http://localhost:4208/remoteEntry.js`,
+    // "mfShopping": "http://localhost:4210/remoteEntry.js",    
+    "mfPayments": `${mfPaymentsUrl}/remoteEntry.js`,
+    "mfShopping": `${mfShoppingUrl}/remoteEntry.js`,
   },
+  // remotes: {
+  //   "mfPayments": `${environment.urlMf1}/remoteEntry.js`,
+  //   "mfShopping": `${environment.urlMf1}/remoteEntry.js`,    
+  // },
+
 
   shared: {
     ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
